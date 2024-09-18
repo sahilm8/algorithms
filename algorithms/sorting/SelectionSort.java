4322c46c42038daca2588ba67467a72b9ef81ae5
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-09-18 14:57:31 +0100

    optimise selection sort

--- algorithms/sorting/SelectionSort.java
+++ algorithms/sorting/SelectionSort.java
@@ -10,15 +10,16 @@ public class SelectionSort {
     public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int minIndex = i;
-            // only compare with the unsorted part of the array
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if (arr[j] < minIndex) {
                     minIndex = j;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
         }
     }
 
