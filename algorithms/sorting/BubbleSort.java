fddf9c7b56dd8bbd1efe509c2221bd4958f365a1
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-09-19 20:52:41 +0100

    format bubbleSort

--- algorithms/sorting/BubbleSort.java
+++ algorithms/sorting/BubbleSort.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  */
 public class BubbleSort {
     public static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
