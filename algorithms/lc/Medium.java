b3d9cc5f96cc1567631a5b3c91d6868708007f58
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-10-22 19:18:33 +0100

    update 3 Sum, add complexities

--- algorithms/lc/Medium.java
+++ algorithms/lc/Medium.java
@@ -1,6 +1,9 @@
 package algorithms.lc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Medium {
      // 1041. Robot Bounded in Circle (Medium) [T = O(n), S = O(1)]
@@ -79,9 +82,47 @@ public class Medium {
         return result.toString();
     }
 
+    // 15. 3Sum (Medium) [T = O(n^2), S = O(1)]
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        // ALWAYS sort for two pointers
+        Arrays.sort(nums);
+        // A counter and two pointers (i, left, right)
+        for (int i = 0; i < nums.length; i++) {
+            // Skip duplicate element
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    result.add(list);
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+    
     public static void main(String[] args) {
         System.out.println(isRobotBounded("GGLLGGLLGG"));
         System.out.println(numDecodings("226"));
         System.out.println(fractionToDecimal(-1, -2147483648));
+        System.out.println(threeSum(new int[] {-1,0,1,2,-1,-4}));
     }
 }
