5702ffd25def4ff4fc36aeefe4310310b083f813
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-10-22 04:29:21 +0100

    update numDecodings, add complexities

--- algorithms/lc/Medium.java
+++ algorithms/lc/Medium.java
@@ -1,7 +1,7 @@
 package algorithms.lc;
 
 public class Medium {
-        // 1041. Robot Bounded in Circle (Medium) [T = O(n), S = O(1)]
+     // 1041. Robot Bounded in Circle (Medium) [T = O(n), S = O(1)]
     public static boolean isRobotBounded(String instructions) {
         int x = 0, y = 0;
         int facing = 0; // N E S W
@@ -21,7 +21,30 @@ public class Medium {
         return false;
     }
 
+    // 91. Decode Ways (Medium) [T = O(n), S = O(n) (due to DP array size)]
+    public static int numDecodings(String s) {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0')
+            return 0;
+        // Dynamic programming: dynamically build up to the result
+        int[] array = new int[s.length() + 1]; // 0 to s.length inclusive
+        // Example: 226 => 1 1 2 3
+        array[0] = 1; // DP convention
+        array[1] = 1; // 1 way to decode a single number
+        for (int i = 2; i <= s.length(); i++) {
+            int singleDigit = Integer.parseInt(s.substring(i - 1, i));
+            int doubleDigit = Integer.parseInt(s.substring(i - 2, i));
+            if (singleDigit >= 1 && singleDigit <= 9) {
+                array[i] += array[i - 1];
+            }
+            if (doubleDigit >= 10 && doubleDigit <= 26) {
+                array[i] += array[i - 2];
+            }
+        }
+        return array[array.length - 1];
+    }
+
     public static void main(String[] args) {
         System.out.println(isRobotBounded("GGLLGGLLGG"));
+        System.out.println(numDecodings("226"));
     }
 }
