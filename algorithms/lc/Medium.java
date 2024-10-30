2e8c84c08236c4e42e527cd944401d21cef32774
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-10-30 01:54:18 +0000

    simplify threeSum

--- algorithms/lc/Medium.java
+++ algorithms/lc/Medium.java
@@ -99,11 +99,7 @@ public class Medium {
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[left]);
-                    list.add(nums[right]);
-                    triplets.add(list);
+                    triplets.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     while (left < right && nums[left] == nums[left + 1])
                         left++;
                     while (left < right && nums[right] == nums[right - 1])
