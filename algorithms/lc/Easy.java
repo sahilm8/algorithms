9cf5bece4d556511ea194d1b9d21915847180504
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-11-28 12:35:52 +0000

    Update mergeTwoLists

--- algorithms/lc/Easy.java
+++ algorithms/lc/Easy.java
@@ -66,7 +66,7 @@ public class Easy {
     }
 
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode(0);
+        ListNode head = new ListNode();
         ListNode current = head;
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
@@ -84,7 +84,7 @@ public class Easy {
         if (list2 != null) {
             current.next = list2;
         }
-        return head.next; // Returning the node after head
+        return head.next;
     }
 
     public static String listNodeToString(ListNode head) {
